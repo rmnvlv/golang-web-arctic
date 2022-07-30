9095ec292d0a3f7fcbff92dc4801e77b93ea2251
Max <2thousandmax@gmail.com>
2022-07-30 23:13:20 +0300

    Добавляет возможность сохранять файлы на яндекс диск

--- disk.go
+++ disk.go
@@ -7,96 +7,81 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
-var resultJson struct {
-	Href string `json:"href"`
+const (
+	YandexDiskAPIURL = "https://cloud-api.yandex.net/v1/disk"
+	ArticlesFolder   = "Articles"
+	AbstractsFolder  = "Tezisi"
+)
+
+var (
+	YandexClientId    = os.Getenv("YANDEX_CLIENT_ID")
+	YandexCallbackURL = os.Getenv("YANDEX_CALLBACK_URL")
+	YandexOAuthToken  = os.Getenv("YANDEX_OAUTH_TOKEN")
+)
+
+type UploadURLResponse struct {
+	OperationId string `json:"operation_id"`
+	URL         string `json:"href"`
+	Method      string `json:"method"`
 }
 
-// func main() {
-// 	err := uploadArticleYandex("./uploadedFiles/myFile.docx", "Articles/newFile.docx")
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// }
+func saveToYandexDisk(file io.Reader, remotePath string) error {
+	logger := log.Default()
 
-func initEnv() {
-	// loads values from .env into the system
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
-}
-
-//Need local path of the file and path of ydisc like docs/pasport.jpg (docs - folder)
-//Two remote paths : Articles/name.doc Thusiss/name.doc
-func uploadArticleYandex(localPath io.Reader, remotePath string) error {
-	initEnv()
-
-	var yandexURL, _ = os.LookupEnv("YANDEX_URL")
-	var token, _ = os.LookupEnv("YANDEX_TOKEN")
-
-	// fmt.Println(token, yandexURL)
-
-	//Read file
-	// data, err := os.Open(localPath)
-	// fmt.Println("End of open data")
-	// if err != nil {
-	// 	return err
-	// }
-
-	//Get url to upload file
-	href, err := getRemoteUrl(remotePath, yandexURL, token)
-	// fmt.Println("End of getRemoteUrl")
-	if err != nil {
-		return err
-	}
-
-	// defer data.Close()
-
-	//Upload file with uploading url
-	request, err := http.NewRequest("PUT", href, localPath)
-	// fmt.Println("End of getting request")
-	if err != nil {
-		return err
-	}
-
-	request.Header.Add("Authorization", fmt.Sprintf("OAuth %s", token))
+	header := http.Header{}
+	header.Add("Authorization", "OAuth "+YandexOAuthToken)
 
 	client := http.Client{}
-	response, err := client.Do(request)
-	// fmt.Println("End of request")
+
+	getUploadURLRequest, err := http.NewRequest("GET",
+		fmt.Sprintf("%s/resources/upload?path=%s&overwrite=true", YandexDiskAPIURL, remotePath), nil)
+	if err != nil {
+		logger.Println(err)
+		return err
+	}
+
+	getUploadURLRequest.Header = header
+
+	response, err := client.Do(getUploadURLRequest)
+	if err != nil {
+		logger.Println(err)
+		return err
+	}
+
+	defer response.Body.Close()
+
+	var getUploadURLResponse UploadURLResponse
+	if err := json.NewDecoder(response.Body).Decode(&getUploadURLResponse); err != nil {
+		logger.Println(err)
+		return err
+	}
+
+	logger.Println(getUploadURLResponse)
+
+	uploadFileRequest, err := http.NewRequest(getUploadURLResponse.Method, getUploadURLResponse.URL, file)
 	if err != nil {
 		return err
 	}
+
+	uploadFileRequest.Header = header
+
+	response, err = client.Do(uploadFileRequest)
+	if err != nil {
+		return err
+	}
+
 	defer response.Body.Close()
 
 	return nil
 }
 
-func apiRequest(path, method, yandexURL, token string) (*http.Response, error) {
-	client := http.Client{}
-	url := fmt.Sprintf("%s/%s", yandexURL, path)
-	request, _ := http.NewRequest(method, url, nil)
-	request.Header.Add("Authorization", fmt.Sprintf("OAuth %s", token))
-	return client.Do(request)
-}
+// func main() {
+// 	f, err := os.Open("go.mod")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-//Get url of reader/writer of Ydisk example:https://uploader34g.disk.yandex.net:443...
-func getRemoteUrl(path, yandexURL, token string) (string, error) {
-	// overwrite - ?
-	response, err := apiRequest(fmt.Sprintf("resources/upload?path=%s&overwrite=true", path), "GET", yandexURL, token)
-	if err != nil {
-		return "", err
-	}
-
-	err = json.NewDecoder(response.Body).Decode(&resultJson)
-	if err != nil {
-		return "", err
-	}
-
-	// fmt.Println(resultJson.Href)
-
-	return resultJson.Href, nil
-}
+// 	saveToYandexDisk(f, "Test/go.mod")
+// }
